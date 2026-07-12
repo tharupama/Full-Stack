abe13c0e584f3ef941c27ac12f9ca96944758fa8
tharupama <tharupama826@gmail.com>
2026-07-12 11:32:23 +0530

    implemented zipkin with micrometer to establish distributed tracking

--- online-book-store-back/AuthService/src/main/java/com/OnlineBookStore/AuthService/controller/LoginController.java
+++ online-book-store-back/AuthService/src/main/java/com/OnlineBookStore/AuthService/controller/LoginController.java
@@ -3,6 +3,7 @@ package com.OnlineBookStore.AuthService.controller;
 import com.OnlineBookStore.AuthService.dto.LoginRequest;
 import com.OnlineBookStore.AuthService.dto.LoginResponse;
 import com.OnlineBookStore.AuthService.service.JwtService;
+import io.micrometer.observation.annotation.Observed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoginController {
     @Autowired
     private JwtService jwtService;
-
+    //@Observed(name = "auth.login")
     @PostMapping("/login")
     public LoginResponse createTokenAndLogin(@RequestBody LoginRequest loginRequest )throws Exception{
         System.out.println(loginRequest);
